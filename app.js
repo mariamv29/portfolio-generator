68a6e9d352d6ed6278d9dc1fbf9f621e84f25f3c
Maria Mireles <mireles335@gmail.com>
2021-06-29 17:48:44 -0500

    end of lesson 1

---
+++ app.js
@@ -0,0 +1,14 @@
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+      }
+    
+      console.log('================');
+    
+      // Is the same as this...
+      profileDataArr.forEach(profileItem => console.log(profileItem));
+    };
+  printProfileData(profileDataArgs);
