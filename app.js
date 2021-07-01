0cf1da34e96cbb041af11e4b95ba310159ade333
Maria Mireles <mireles335@gmail.com>
2021-06-30 19:45:57 -0500

    creating .gitignore/file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const fs = require('fs');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
